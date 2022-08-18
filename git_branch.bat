5b35f57cf0b00f7defd0d0451d2502cd7cc8b17d
johnbp78 <107950802+johnbp78@users.noreply.github.com>
2022-08-18 14:41:10 -0500

    updated to latest changes

--- git_branch.bat
+++ git_branch.bat
@@ -1,5 +1,6 @@
 echo off
 
+if [%1] == [] goto usage
 rem set the variable RESULT to empty string
 set RESULT=
 for /f %%i in ('git status -s') do set RESULT=%%i
@@ -10,8 +11,7 @@ git checkout %~1/%2
 git fetch origin 
 git reset --hard origin/%~1/%2
 rundll32 user32.dll,MessageBeep
-@echo Does everything look ok? Press any key to create branch %3 
-pause
+
 
 git checkout -b users/pearj01/%1/%2/%3
 
@@ -21,14 +21,16 @@ exit /B 1
 echo on
 
 :usage
-@echo ------------------------------------------------------------------
-@echo %0 creates a new feature branch after fetching latest code
-@echo ------------------------------------------------------------------
+@echo -----------------------------------------------------------------------------
+@echo %0 creates a new feature branch
+@echo     - checks if working tree is clean
+@echo 	  - fetches latest code
+@echo -----------------------------------------------------------------------------
 @echo Usage: %0 param%%1 param%%2 param%%3
-@echo       param%%1 the relative path of the branch to operate on
-@echo       param%%2 the branch to operate on
-@echo       param%%3 the name of the feature branch ex. defect99999
-@echo ------------------------------------------------------------------
+@echo       param%%1 the relative path of the branch to operate on. example: release
+@echo       param%%2 the branch to operate on. example: 15.0
+@echo       param%%3 the name of the feature branch. example defect99999
+@echo -----------------------------------------------------------------------------
 
 
 exit /B 1
@@ -36,9 +38,10 @@ echo on
 
 
 :uncommitted
-@echo --------------------------------------------------------------
+@echo -------------------------------------------------------------------------
 @echo error uncommitted changes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-@echo --------------------------------------------------------------
+@echo *******exiting script before we lose anything important*******
+@echo -------------------------------------------------------------------------
 
 exit /b 1
 echo on
