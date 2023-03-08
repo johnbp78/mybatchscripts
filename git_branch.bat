ee7ed35d84f6b1535fa0f2c52853cc3bcd3d12a7
john.b.pearson <john.b.pearson@rcis.com>
2023-03-08 11:29:45 -0600

    More updates
    Some enhancements

--- git_branch.bat
+++ git_branch.bat
@@ -35,7 +35,7 @@ git checkout %~1/%2
 git fetch origin 
 git reset --hard origin/%~1/%2
 rundll32 user32.dll,MessageBeep
-git checkout -b users/%username%/%1/%2/%3
+git checkout -b users/%git_username%/%1/%2/%3
 goto exitSuccess
 
 
@@ -51,7 +51,7 @@ git checkout %~1
 git fetch origin 
 git reset --hard origin/%~1
 rundll32 user32.dll,MessageBeep
-git checkout -b users/%username%/%1/%2
+git checkout -b users/%git_username%/%1/%2
 goto exitSuccess
 
 
